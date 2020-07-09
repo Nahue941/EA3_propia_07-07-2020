4fa6140c79fa5662c12fdb55a02935686ac43b47
Nahue941 <nahuelpaivag@gmail.com>
2020-07-09 15:12:36 -0300

    v4

--- funciones.c
+++ funciones.c
@@ -40,69 +40,28 @@ char* strcpy_(char *s1, const char *s2)
     return (char*)aux;
 }
 
-tNodo* buscarmayorocurrencias(tNodo* faltante)
+void nodoMayorOcurrencia (tNodo* nodMov, char* clave, int(*comparar)(const void*, const void*))
 {
-    int cont=0,max=0;
-    tNodo* aux= faltante;
-    while(faltante->sig)
-    {
-        const tNodo* mov= faltante;
-        while(mov)
-        {
-            if(strcmp_(faltante->info,mov->info)==0)
-                cont++;
-            mov = mov->sig;
+    int mayorApariciones=0, // Cuenta cuantas veces aparece el que aparecio mas veces
+        contadorApariciones=0;// contador apariciones individual por clave
+
+    tNodo* nodReset = nodMov;//Nodo que tengo para volver al incio de la lista.
+    char cadComparada[11];//En esta cadena voy a ir guardando las claves que estoy contando sus apariciones
+
+    while (nodMov->sig){//el ultimo ya lo voy a tener contemplado por eso no lo analizo
+        strcpy_(cadComparada, nodMov->info);//Tengo la clave del primer nodo
+        while (nodMov){
+            if (!comparar(cadComparada, nodMov->info))//si hubo coincidencia
+                contadorApariciones++;//incremento el contador de apariciones
+            nodMov = nodMov->sig;//avanzo el puntero
         }
-        if(cont>max)
-        {
-            max=cont;
-            strcpy_(aux->info,faltante->info);
+        if (contadorApariciones>mayorApariciones){
+            mayorApariciones = contadorApariciones;
+            strcpy_(clave, cadComparada);//guardo en clave la cadena que por el momento aparecion mas veces
         }
-        cont=0;
-        faltante = faltante->sig;
+        nodMov = nodReset = nodReset -> sig;//Avanzo el nodo Reset e igualo al nodo movimiento a la siguiente sub lista
+        contadorApariciones = 0;// Reseteo el contador de apariciones
     }
-    faltante = aux;
-    return aux;
-}
-
-void recorreryordenar( tNodo* faltante,tNodo* act,tNodo* Ordenado)
-{
-    while(faltante!=act)
-        faltante=faltante->sig;
-    while(faltante)
-    {
-       if((strcmp_(act->info,faltante->info)==0) && (faltante!=Ordenado))
-       {
-           tNodo *cambiar=faltante,
-                 *ant=cambiar->ant,
-                 *sig=cambiar->sig;
-                ant->sig=sig;
-                sig->ant=ant;
-                faltante=sig;
-            if(strcmp_(Ordenado->info,cambiar->info) == 0)
-            {
-                cambiar->ant = Ordenado;
-                cambiar->sig = Ordenado->sig;
-                Ordenado->sig->ant = cambiar;
-                Ordenado->sig = cambiar;
-                Ordenado = Ordenado->sig;
-            }
-            else
-            {
-                cambiar->sig = Ordenado;
-                cambiar->ant = NULL;
-                Ordenado->ant = cambiar;
-                Ordenado = Ordenado->ant;
-            }
-
-       }
-       else
-       {
-           faltante = faltante->sig;
-       }
-
-    }
-    faltante = Ordenado;
 }
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 /**//* FUNCIONES Y PRIMITIVAS A DESARROLLAR                               *//**/
@@ -170,12 +129,57 @@ int  mostrarDeIzqADer_MIO(const tLista *p,
     return cant;
 }
 
-/**
+
 void ordenarLista_MIO(tLista *p, int (*comparar)(const void *, const void *))
 {
+    if (!*p)
+        return;
+
+    tNodo* nodMov = *p;//Nodo con el cual voy a ir recorriendo la lista desordenada
+    char auxCad [11];//Cadena que voy a usar para la comparacion
+    while (nodMov->ant)//me muevo al principio de la lista
+        nodMov = nodMov->ant;
+
+    tNodo* nodFirst = nodMov;//NODFIRST es el nodo que va a apuntar al primer Nodo de la lista desordenada
+    tNodo* nodOrd = NULL;//El Nodo Ordenado va a ser NULL para que sea el anterior del primer ordenado
+
+    while (nodFirst->sig){
+        nodoMayorOcurrencia(nodFirst, auxCad, comparar);//BIEN
+
+        while(nodMov){
+            if(!comparar(auxCad, nodMov->info)){
+                if(nodMov==nodFirst)//En caso de que este parado en el primero y este sea uno a desenganchar muevo el puntero first.
+                    nodFirst=nodFirst->sig;
+                //-----desenganche-del-nodo-----//
+                tNodo* ant = nodMov->ant,* sig = nodMov->sig;
+                //USAR NODO SIG COMO MARCA//
+                if (ant)
+                    ant->sig = sig;
+                if (sig){
+                    sig->ant = ant;
+                    *p = sig;
+                }
+                else
+                    *p = ant;
+                //-----desenganche-del-nodo-----//
+                nodMov->ant = nodOrd;//EL ANTERIOR DEL NODO QUE DESENGANCHO VA A SER LO QUE APUNTE ORDEANDO
+                if(nodOrd)//En caso de que el puntero no sea nulo tengo que apuntar su sig al nodo que le engancho
+                    nodOrd->sig = nodMov;
+                nodOrd = nodMov;
+                nodMov = sig;//Resetie el nodo movimiento para poder seguir buscando desde aca y no tener que recorrer lo que ya recorri
+                nodOrd->sig = nodFirst;//engancho el ultimo de la lista ordenada ->
+                nodFirst->ant = nodOrd;//-> con el primero de la lista desordenada.
+            }
+            else
+                nodMov = nodMov->sig;//Avanzo el nodo movimiento
+        }
+        nodMov = nodFirst;//Muevo al principio de la lista ordenada el nodMov para volver a arrancar
+       ///ESTO TENGO QUE HACERLO SOLO SI EL PRIMERO QUE ENGANCHE ERA NODO A MOVER!!!!!!!!!!!///
+        //nodMov = nodMov->sig //AVANZO EL PUNTERO MOVIMIENTO
+    }
 
 }
-*/
+
 
 int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
                                    int (*comparar)(const void *, const void *),
@@ -202,7 +206,7 @@ int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
         strcpy_(aux.dni, nodMov->info);//copio el dni en la estructura auxiliar
 
         mostrar(NULL, fp);//Imprimo el encabezado
-        cantMaterias = 0;
+        cantMaterias = 0;//Reseteo la cantidad de materias por alumno, variable que uso para el promedio
 
 
         do{//Utilizo un DO porque si hay lista quiere decir que hay un puntero si o si. Por lo tanto aunque sea 1 nodo tendre.
@@ -220,7 +224,7 @@ int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
 
             cantMaterias++;
             contTotal++;
-        }while (nodMov && !comparar(aux.dni,nodMov->info));//Mientras que sean el mismo DNI sigo acumulando
+        }while (nodMov && !comparar(aux.dni,nodMov->info));//Mientras haya nodo y que sean el mismo DNI sigo acumulando
 
         mostrar2(&aux,cantMaterias, fp);
     }
