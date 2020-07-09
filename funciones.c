330a206186f5e100ba7433d56bd6ae53d428a74a
Nahue941 <nahuelpaivag@gmail.com>
2020-07-08 21:48:33 -0300

    v3

--- funciones.c
+++ funciones.c
@@ -11,6 +11,14 @@
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 /**//**//* ACÁ DEBE DESARROLLAR LAS FUNCIONES Y PRIMITIVAS PEDIDAS    *//**//**/
 /**//**//* ADEMÁS DE CUALQUIER OTRA FUNCIÓN QUE SE REQUIERA           *//**//**/
+int strcmp_(const char* s1, const char* s2)
+{
+    while (*s1 && *s1==*s2){
+        s1++;
+        s2++;
+    }
+    return * (char*)s1-* (char*)s2;
+}
 
 int promedio (float total, float k)//total / k, donde k es la cantidad de materias
 {
@@ -32,6 +40,70 @@ char* strcpy_(char *s1, const char *s2)
     return (char*)aux;
 }
 
+tNodo* buscarmayorocurrencias(tNodo* faltante)
+{
+    int cont=0,max=0;
+    tNodo* aux= faltante;
+    while(faltante->sig)
+    {
+        const tNodo* mov= faltante;
+        while(mov)
+        {
+            if(strcmp_(faltante->info,mov->info)==0)
+                cont++;
+            mov = mov->sig;
+        }
+        if(cont>max)
+        {
+            max=cont;
+            strcpy_(aux->info,faltante->info);
+        }
+        cont=0;
+        faltante = faltante->sig;
+    }
+    faltante = aux;
+    return aux;
+}
+
+void recorreryordenar( tNodo* faltante,tNodo* act,tNodo* Ordenado)
+{
+    while(faltante!=act)
+        faltante=faltante->sig;
+    while(faltante)
+    {
+       if((strcmp_(act->info,faltante->info)==0) && (faltante!=Ordenado))
+       {
+           tNodo *cambiar=faltante,
+                 *ant=cambiar->ant,
+                 *sig=cambiar->sig;
+                ant->sig=sig;
+                sig->ant=ant;
+                faltante=sig;
+            if(strcmp_(Ordenado->info,cambiar->info) == 0)
+            {
+                cambiar->ant = Ordenado;
+                cambiar->sig = Ordenado->sig;
+                Ordenado->sig->ant = cambiar;
+                Ordenado->sig = cambiar;
+                Ordenado = Ordenado->sig;
+            }
+            else
+            {
+                cambiar->sig = Ordenado;
+                cambiar->ant = NULL;
+                Ordenado->ant = cambiar;
+                Ordenado = Ordenado->ant;
+            }
+
+       }
+       else
+       {
+           faltante = faltante->sig;
+       }
+
+    }
+    faltante = Ordenado;
+}
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 /**//* FUNCIONES Y PRIMITIVAS A DESARROLLAR                               *//**/
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
@@ -103,7 +175,7 @@ void ordenarLista_MIO(tLista *p, int (*comparar)(const void *, const void *))
 {
 
 }
- **/
+*/
 
 int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
                                    int (*comparar)(const void *, const void *),
@@ -112,49 +184,48 @@ int  vaciarMostrarDetalleYProm_MIO(tLista *p, FILE *fp,
                                    void (*mostrar)(const void *, FILE *),
                                    void (*mostrar2)(const void *, int, FILE *))
 {
-    int contTotal = 0, cantMaterias;//contador de materias por alumno
-    while(*p){
-        tFinal aux;//lo voy a usar para acumular y comparar
-        tFinal* paux = &aux;
+    if (!*p)//Controlo que la lista no este vacia
+        return 0;
 
-        tNodo* nodoAct = *p;
+    int contTotal = 0, cantMaterias;//contador de materias por alumno
+    tNodo* nodMov = *p;//Una vez que tengo el puntero a lista en el principio lo igualo al nodo
+
+    while (nodMov->ant)//me muevo al principio de la lista
+        nodMov = nodMov->ant;
+
+    while (nodMov){
+        tFinal aux;
+        tFinal* pAux = &aux;
+
 
         aux.calif = 0;//seteo en 0 las calificaciones. Las voy a usar como acumuladores
-        strcpy_(aux.dni, nodoAct->info);//copio el dni en la estructura auxiliar
+        strcpy_(aux.dni, nodMov->info);//copio el dni en la estructura auxiliar
 
         mostrar(NULL, fp);//Imprimo el encabezado
         cantMaterias = 0;
+
+
         do{//Utilizo un DO porque si hay lista quiere decir que hay un puntero si o si. Por lo tanto aunque sea 1 nodo tendre.
 
-            acumular((void*)&paux,NULL, nodoAct->info, (unsigned)NULL);///BIEN
 
-            mostrar((*p)->info,fp);
-            /////eliminando nodo/////
-            tNodo* elimAnt = nodoAct->ant,* elimSig = nodoAct->sig;
+            acumular((void*)&pAux,NULL, nodMov->info, (unsigned)NULL);///BIEN
+            mostrar(nodMov->info,fp);///BIEN
 
-            if(elimAnt){
-                elimAnt->sig = elimSig;
-                *p = elimAnt;
-            }
-            if(elimSig)
-                elimSig->sig = elimAnt;
+            tNodo* nodAux = nodMov->sig;
 
-            else
-                *p = elimAnt;
-            free(nodoAct->info);
-            free(nodoAct);
+            free(nodMov->info);
+            free(nodMov);
+
+            nodMov = nodAux;
 
             cantMaterias++;
             contTotal++;
-        }while (comparar(aux.dni,nodoAct->info));//Mientras que sean el mismo DNI sigo acumulando
-        /**AL PARECER ESTOY PASANDO MAL EL SEGUNDO ARGUMENTO. CON LA FUNCION DE LOPEZ LA FUNCION DE COMPRAR FUNCIONA PERO
-        CON LA MIA NO. REEVER QUE PARAMETROS PASO.**/
+        }while (nodMov && !comparar(aux.dni,nodMov->info));//Mientras que sean el mismo DNI sigo acumulando
 
         mostrar2(&aux,cantMaterias, fp);
     }
+    *p = NULL;//Como la lista esta vacia seteo a la "fuerza" el puntero de la lista a NULL
     return contTotal;
 }
-
-
 /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
 
